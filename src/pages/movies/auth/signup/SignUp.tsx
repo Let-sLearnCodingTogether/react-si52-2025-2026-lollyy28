ba6493e0fde12d7e24bfe1390cae64e9f9bbea87
lollyy28 <jennychristine2806@gmail.com>
2025-11-29 18:41:23 +0700

    .

---
+++ src/pages/movies/auth/signup/SignUp.tsx
@@ -0,0 +1,77 @@
+import { useState, type ChangeEvent, type FormEvent } from "react"
+import { Button, Form } from "react-bootstrap"
+import ApiClient from "../../../../utils/ApiClient"
+
+interface signform{
+    username: string,
+    email: string,
+    password: string
+}
+
+function SignUp() {
+    const [form, setForm] = useState<signform>({
+        username: "",
+        email: "",
+        password: ""
+    })
+
+    const onHandleChange = (event : ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = event.target
+
+        setForm({
+            ...form,
+            [name]: value
+        })
+
+    }
+
+    const onSubmit = async (event : FormEvent<HTMLInputElement>) => {
+        event.preventDefault();
+
+        try {
+            const response = await ApiClient.post("/signup", form)
+
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return <div className="container mex-auto">
+        <Form>
+                <Form.Group className="mb-3" controlId="formUsername">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control 
+                    value = {form.username}
+                    onChange={onHandleChange}
+                    name="username"
+                    type="text" 
+                    placeholder="Username"/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control 
+                    value = {form.email}
+                    onChange={onHandleChange}
+                    name="email"
+                    type="text" 
+                    placeholder="email"/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control 
+                    value = {form.password}
+                    onChange={onHandleChange}
+                    name="password"
+                    type="text" 
+                    placeholder="password"/>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Sign Up
+                </Button>
+        </Form>
+        </div>
+
+}
+
+export default SignUp
